57b4f0292c981960a4a3fca87c334646f85efe4a
Nmudassar <nadia.syed77@gmail.com>
2026-07-08 23:00:36 +0100

    feat(pomodoro): add save button event listener and capture timer settings

---
+++ assets/js/pomodoro-setting.js
@@ -0,0 +1,4 @@
+const savePomodoro = document.getElementById("savePomodoro");
+savepomodoro.addEventListener("click", function()) {
+    const focusTime = document.getElementById ("focusTime").value;
+}
