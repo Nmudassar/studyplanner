6f84872467bc22138cf5e66addac311b2153e540
Nmudassar <nadia.syed77@gmail.com>
2026-07-09 00:04:09 +0100

    Save timer settings and redirect to dashboard

--- assets/js/pomodoro-setting.js
+++ assets/js/pomodoro-setting.js
@@ -1,4 +1,7 @@
 const savePomodoro = document.getElementById("savePomodoro");
-savepomodoro.addEventListener("click", function()) {
+savepomodoro.addEventListener("click", function() {
     const focusTime = document.getElementById ("focusTime").value;
-}
+    localStorage.setItem("focusTime", focusTime);
+
+    window.location.href = "02-dashboard.html";
+});
