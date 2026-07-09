da1b4957149acf3dcc4da02bf10fa48b513c2862
Nmudassar <nadia.syed77@gmail.com>
2026-07-09 14:40:11 +0100

    feat(sidebar): implement Activities submenu toggle functionality

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,11 +1,24 @@
+// Function to show or hide the Activities submenu
+function toggleActivities(event) {
+
+    // Prevents the link (<a>) from opening its default page
+    event.preventDefault();
+
+    // Finds the HTML element with the id "activitiesMenu"
+    const menu = document.getElementById("activitiesMenu");
+
+    // Adds the "show" class if it doesn't exist,
+    // or removes it if it already exists
+    menu.classList.toggle("show");
+}
+
+
+
+
+
 const settingsBtn = document.getElementById("settingsBtn");
 
 settingsBtn.addEventListener("click", function () {
     window.location.href = "11-pomodoro-settings.html";
 });
 
-const settingsBtn = document.getElementById("settingsBtn");
-
-settingsBtn.addEventListener("click", function () {
-    window.location.href = "11-pomodoro-settings.html";
-});
