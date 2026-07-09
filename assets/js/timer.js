aa480b8c51b8f82ddc6335ad0bb4e1990fdf21de
Nmudassar <nadia.syed77@gmail.com>
2026-07-09 01:03:39 +0100

    Add timer display function

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -12,3 +12,15 @@ const timerDisplay = document.getElementById("timer");
 const startBtn = document.getElementById("startBtn");
 const resetBtn = document.getElementById("resetBtn");
 
+/*=======2 – Create timer display function=======*/
+// Display timer on screen
+function updateDisplay() {
+    const minutes = Math.floor(timer / 60);
+    const seconds = timer % 60;
+
+    timerDisplay.textContent =
+        `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
+}
+
+// Show saved time when dashboard loads
+updateDisplay();
