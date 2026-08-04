eed99f2f0419ccb216bdc19d636e10cd08a855f5
Nmudassar <nadia.syed77@gmail.com>
2026-08-05 00:57:48 +0100

    Fix signup login page layout and input interaction

--- assets/js/login.js
+++ assets/js/login.js
@@ -17,3 +17,49 @@ const emailInput = document.getElementById("email-input");
 const passwordInput = document.getElementById("password-input");
 
 const repeatPasswordInput = document.getElementById("repeat-password-input");
+/* ==================================================
+   Register New User
+================================================== */
+
+signupBtn.addEventListener("click", function () {
+  const firstname = firstnameInput.value.trim();
+
+  const email = emailInput.value.trim();
+
+  const password = passwordInput.value.trim();
+
+  const repeatPassword = repeatPasswordInput.value.trim();
+
+  // Check empty fields
+
+  if (
+    firstname === "" ||
+    email === "" ||
+    password === "" ||
+    repeatPassword === ""
+  ) {
+    alert("Please complete all fields");
+
+    return;
+  }
+
+  // Check password match
+
+  if (password !== repeatPassword) {
+    alert("Passwords do not match");
+
+    return;
+  }
+
+  const user = {
+    firstname: firstname,
+
+    email: email,
+
+    password: password,
+  };
+
+  localStorage.setItem("studySmartUser", JSON.stringify(user));
+
+  alert("Account created successfully");
+});
