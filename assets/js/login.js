33733490744df300b3df27bd862fb46d4a40e259
Nmudassar <nadia.syed77@gmail.com>
2026-08-05 00:49:53 +0100

    decalring variables

---
+++ assets/js/login.js
@@ -0,0 +1,19 @@
+"use strict";
+
+/* ==================================================
+   Get HTML Elements
+================================================== */
+
+const signupBtn = document.getElementById("signupBtn");
+
+const loginBtn = document.getElementById("loginBtn");
+
+const resetPassword = document.getElementById("resetPassword");
+
+const firstnameInput = document.getElementById("firstname-input");
+
+const emailInput = document.getElementById("email-input");
+
+const passwordInput = document.getElementById("password-input");
+
+const repeatPasswordInput = document.getElementById("repeat-password-input");
